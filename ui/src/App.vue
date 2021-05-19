95f59c274f1c74038614ae7915eccb6e8a9f62b0
Karl Benedict <kbene@unm.edu>
2021-05-19 20:29:14 +0000

    added login components

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -1,8 +1,8 @@
 <template>
-    <Navbar></Navbar>
-    <router-view></router-view>
-    <hr />
-    <Footer></Footer>
+  <Navbar></Navbar>
+  <router-view></router-view>
+  <hr />
+  <Footer></Footer>
 </template>
 
 <script>
@@ -10,21 +10,51 @@ import Navbar from "./components/Navbar.vue";
 import Footer from "./components/Footer.vue";
 
 export default {
-    name: "App",
-    components: {
-        Navbar,
-        Footer,
-    },
+  name: "App",
+  components: {
+    Navbar,
+    Footer,
+  },
 };
 </script>
 
 <style>
 #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    /* text-align: center; */
-    color: #2c3e50;
-    margin-top: 60px;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  /* text-align: center; */
+  color: #2c3e50;
+  margin-top: 60px;
+  width: 95%;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+h2 {
+  margin-bottom: 4px;
+  margin-top: 3px;
+}
+
+h3 {
+  margin-top: 3px;
+  margin-bottom: 0px;
+}
+
+.blog-item {
+  display: flex;
+  flex-direction: column;
+}
+
+.blog-date {
+  font-size: x-small;
+  padding: 0px;
+  margin-top: 0px;
+  margin-bottom: 0px;
+}
+
+.blog-content {
+  padding: 0px 30px 10px;
+  flex: 1 1 200px;
 }
 </style>
--- ui/src/main.js
+++ ui/src/main.js
@@ -1,24 +1,27 @@
-import { createApp } from 'vue'
-import { createRouter, createWebHistory } from 'vue-router'
-import App from './App.vue'
-import About from './components/About.vue'
-import Contribute from './components/Contribute.vue'
-import Home from './components/Home.vue'
-import Search from './components/Search.vue'
-import Support from './components/Support.vue'
+import { createApp } from "vue";
+import { createRouter, createWebHistory } from "vue-router";
+import App from "./App.vue";
+import About from "./components/About.vue";
+import Contribute from "./components/Contribute.vue";
+import Home from "./components/Home.vue";
+import Search from "./components/Search.vue";
+import Support from "./components/Support.vue";
+import Resource from "./components/Resource.vue";
+import Login from "./components/Login.vue";
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes: [
-        { path: '/', name: 'Search', component: Search },
-        { path: '/home', name: 'Home', component: Home },
-        { path: '/about', name: 'About', component: About },
-        { path: '/contribute', name: 'Contribute', component: Contribute },
-        { path: '/search', name: 'Search', component: Search },
-        { path: '/support', name: 'Support', component: Support },
-    ]
+  history: createWebHistory(),
+  routes: [
+    { path: "/", redirect: "/home" },
+    { path: "/home", name: "Home", component: Home },
+    { path: "/about", name: "About", component: About },
+    { path: "/contribute", name: "Contribute", component: Contribute },
+    { path: "/search", name: "Search", component: Search },
+    { path: "/support", name: "Support", component: Support },
+    { path: "/resource/:id", name: "Resource", component: Resource },
+    { path: "/login", name: "Login", component: Login },
+  ],
 });
 
 const app = createApp(App);
-app.use(router),
-app.mount('#app')
+app.use(router), app.mount("#app");
