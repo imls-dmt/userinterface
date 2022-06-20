f1f0c13c1cc90813aad2ff7664dfafa2538aa1af
Karl Benedict <kbene@unm.edu>
2022-06-20 09:23:28 -0600

    groups updating fixed

--- ui/src/components/Login.vue
+++ ui/src/components/Login.vue
@@ -1,124 +1,120 @@
 <template>
-<div class="col-md-12">
-	<span v-if="loggedin">
-		<p>You are logged in as: {{ username }}</p>
-		<p>You are a member of the following DMTC groups:</p>
-		<ul>
-			<li v-for="(group,index) in groups" :key="index">{{ group }} </li>
-		</ul>
-		<p>To request that you be added to additional DMTC groups please <a href="mailto:clearinghouseEd@esipfed.org?subject=Request for DMTC group change">email us</a>.</p>
-		<Form @submit="handleLogout">
-			<div class="form-group">
-				<button class="btn btn-primary btn-block">
-				<span>Logout</span>
-				</button>
-			</div>
-		</Form>
-	</span>
-	<span v-else>
-		<div class="card card-container">
-			<Form @submit="handleLogin" :validation-schema="schema">
-				<div class="form-group">
-					<label for="username">Username</label>
-					<Field name="username" type="text" class="form-control" />
-					<ErrorMessage name="username" class="error-feedback" />
-				</div>
-				<div class="form-group">
-					<label for="password">Password</label>
-					<Field name="password" type="password" class="form-control" />
-					<ErrorMessage name="password" class="error-feedback" />
-				</div>	
-				<div class="form-group">
-					<button class="btn btn-primary btn-block" :disabled="loading">
-					<span
-						v-show="loading"
-						class="spinner-border spinner-border-sm"
-					></span>
-					<span>Login</span>
-					</button>
-				</div>
-				<div class="form-group">
-					<div v-if="message" class="alert alert-danger" role="alert">
-						{{ message }}
-					</div>
-				</div>
-			</Form>
-		</div>
-	</span>
-</div>
+  <div class="col-md-12">
+    <span v-if="loggedin">
+      <p>You are logged in as: {{ username }}</p>
+      <p>You are a member of the following DMTC groups:</p>
+      <ul>
+        <li v-for="(group, index) in groups" :key="index">{{ group }}</li>
+      </ul>
+      <p>
+        To request that you be added to additional DMTC groups please
+        <a
+          href="mailto:clearinghouseEd@esipfed.org?subject=Request for DMTC group change"
+          >email us</a
+        >.
+      </p>
+      <Form @submit="handleLogout">
+        <div class="form-group">
+          <button class="btn btn-primary btn-block">
+            <span>Logout</span>
+          </button>
+        </div>
+      </Form>
+    </span>
+    <span v-else>
+      <div class="card card-container">
+        <Form @submit="handleLogin" :validation-schema="schema">
+          <div class="form-group">
+            <label for="username">Username</label>
+            <Field name="username" type="text" class="form-control" />
+            <ErrorMessage name="username" class="error-feedback" />
+          </div>
+          <div class="form-group">
+            <label for="password">Password</label>
+            <Field name="password" type="password" class="form-control" />
+            <ErrorMessage name="password" class="error-feedback" />
+          </div>
+          <div class="form-group">
+            <button class="btn btn-primary btn-block" :disabled="loading">
+              <span
+                v-show="loading"
+                class="spinner-border spinner-border-sm"
+              ></span>
+              <span>Login</span>
+            </button>
+          </div>
+          <div class="form-group">
+            <div v-if="message" class="alert alert-danger" role="alert">
+              {{ message }}
+            </div>
+          </div>
+        </Form>
+      </div>
+    </span>
+  </div>
 </template>
-	
+
 <script>
-	import { Form, Field, ErrorMessage } from "vee-validate";
-	import * as yup from "yup";
-	import { mapGetters } from 'vuex';
+import { Form, Field, ErrorMessage } from "vee-validate";
+import * as yup from "yup";
+import { mapGetters } from "vuex";
 
-	export default {
-		name: "Login",
-		
-		components: {
-			Form,
-			Field,
-			ErrorMessage,
-		},
-		
-		data() {
-			const schema = yup.object().shape({
-				username: yup.string().required("Username is required!"),
-				password: yup.string().required("Password is required!"),
-			});
-	
-			return {
-				loading: false,
-				message: "",
-				schema,
-			};
-		},
+export default {
+  name: "Login",
 
-		computed: {
-			...mapGetters([
-				'loggedin',
-				'username',
-				'groups'
-			])
-		},
-		
-		methods: {
-			handleLogin(user) {
-				this.loading = true;
-				//console.log("username: " + user.username)
-				//console.log(user);
-				this.$store.dispatch("login", user).then(
-					() => {
-						//console.log(response)
-						this.loading = false;
-						//this.$router.push("/profile");
-					},
-					(error) => {
-						this.loading = false;
-						this.message =
-							(error.response &&
-							error.response.data &&
-							error.response.data.message) ||
-							error.message ||
-							error.toString();
-					}
-				);
-				this.$store.dispatch("getGroups").then(
-					() => {
-						console.log("getting the user's groups")
-					}
-				);
-					
-			},
-			handleLogout() {
-				this.$store.dispatch("logout");
-				
-			}
-		},
-	};
+  components: {
+    Form,
+    Field,
+    ErrorMessage,
+  },
+
+  data() {
+    const schema = yup.object().shape({
+      username: yup.string().required("Username is required!"),
+      password: yup.string().required("Password is required!"),
+    });
+
+    return {
+      loading: false,
+      message: "",
+      schema,
+    };
+  },
+
+  computed: {
+    ...mapGetters(["loggedin", "username", "groups"]),
+  },
+
+  methods: {
+    handleLogin(user) {
+      this.loading = true;
+      //console.log("username: " + user.username)
+      //console.log(user);
+      this.$store.dispatch("login", user).then(
+        () => {
+          //console.log(response)
+          this.loading = false;
+          //this.$router.push("/profile");
+          this.$store.dispatch("getGroups").then(() => {
+            console.log("getting the user's groups");
+          });
+        },
+        (error) => {
+          this.loading = false;
+          this.message =
+            (error.response &&
+              error.response.data &&
+              error.response.data.message) ||
+            error.message ||
+            error.toString();
+        }
+      );
+    },
+    handleLogout() {
+      this.$store.dispatch("logout");
+    },
+  },
+};
 </script>
-	
-<style scoped>
-</style>
-	
+
+<style scoped></style>
--- ui/src/store/index.js
+++ ui/src/store/index.js
@@ -1,105 +1,103 @@
 // import { createStore } from "vuex";
 // import { auth } from "./auth.module";
-// 
+//
 // const store = createStore({
 // 	modules: {
 // 		auth,
 // 	},
 // });
-// 
+//
 // export default store;
 
 import Vuex from "vuex";
-import AuthService from '../services/auth.service';
-import UserService from '../services/user.service';
+import AuthService from "../services/auth.service";
+import UserService from "../services/user.service";
 
 const initialState = {
-	working: false,
-	user: {
-		loggedIn: false,
-		username: "",
-	},
-	groups: [],	
-}
+  working: false,
+  user: {
+    loggedIn: false,
+    username: "",
+  },
+  groups: [],
+};
 
 export default new Vuex.Store({
-	namespaced: true,
-	state: initialState,
-	getters: {
-		fullState: state => state,
-		user: state => state.user,
-		loggedin: state => state.user.loggedIn,
-		username: state => state.user.username,
-		groups: state => state.groups,
-		working: state => state.working,
-	},
-	actions: {
-		login({ commit }, user) {
-			commit('setWorking', true);
-			return AuthService.login(user).then(
-				result => {
-					if (result.status == "success") {
-						commit('loginSuccess');
-						commit('setUsername', user.username);
-						commit('setWorking', false);
-						return Promise.resolve(user);
-					} else {
-						commit('loginFailure');
-						commit('setWorking', false);
-						return Promise.resolve(user);
-					}
-				},
-				error => {
-					commit('loginFailure');
-					commit('setWorking', false);
-					return Promise.reject(error);
-				}
-			);
-		},
-		logout({ commit }) {
-			commit('setWorking', true);
-			commit('logout');
-			commit('setWorking', false);
-		},
-		getGroups({ commit }) {
-			commit('setWorking', true);
-			return UserService.getGroups().then(
-				result => {
-					console.log(result);
-					commit('setGroups', result);
-					commit('setWorking', false);
-				}
-			)
-		}
-	},
-	mutations: {
-		loginSuccess(state) {
-			state.user.loggedIn = true;
-			console.log(state);
-		},
-		loginFailure(state) {
-			state.user.loggedIn = false;
-			state.user.username = null;
-			state.user.groups = [];
-			console.log(state)
-		},
-		logout(state) {
-			state.user.loggedIn = false;
-			state.user.username = null;
-			state.groups = [];
-			console.log(state)
-		},
-		setUsername(state, username) {
-			state.user.username = username;
-			console.log(state)
-		},
-		setGroups (state, groups) {
-			state.groups = groups.data.groups;
-			console.log(state)
-		},
-		setWorking (state, workingState) {
-			state.working = workingState;
-			console.log(state)
-		}
-	}
+  namespaced: true,
+  state: initialState,
+  getters: {
+    fullState: (state) => state,
+    user: (state) => state.user,
+    loggedin: (state) => state.user.loggedIn,
+    username: (state) => state.user.username,
+    groups: (state) => state.groups,
+    working: (state) => state.working,
+  },
+  actions: {
+    login({ commit }, user) {
+      commit("setWorking", true);
+      return AuthService.login(user).then(
+        (result) => {
+          if (result.status == "success") {
+            commit("loginSuccess");
+            commit("setUsername", user.username);
+            commit("setWorking", false);
+            return Promise.resolve(user);
+          } else {
+            commit("loginFailure");
+            commit("setWorking", false);
+            return Promise.resolve(user);
+          }
+        },
+        (error) => {
+          commit("loginFailure");
+          commit("setWorking", false);
+          return Promise.reject(error);
+        }
+      );
+    },
+    logout({ commit }) {
+      commit("setWorking", true);
+      commit("logout");
+      commit("setWorking", false);
+    },
+    getGroups({ commit }) {
+      commit("setWorking", true);
+      return UserService.getGroups().then((result) => {
+        console.log(result);
+        commit("setGroups", result);
+        commit("setWorking", false);
+      });
+    },
+  },
+  mutations: {
+    loginSuccess(state) {
+      state.user.loggedIn = true;
+      console.log(state);
+    },
+    loginFailure(state) {
+      state.user.loggedIn = false;
+      state.user.username = null;
+      state.user.groups = [];
+      console.log(state);
+    },
+    logout(state) {
+      state.user.loggedIn = false;
+      state.user.username = null;
+      state.groups = [];
+      console.log(state);
+    },
+    setUsername(state, username) {
+      state.user.username = username;
+      console.log(state);
+    },
+    setGroups(state, groups) {
+      state.groups = groups.data.groups;
+      console.log(state);
+    },
+    setWorking(state, workingState) {
+      state.working = workingState;
+      console.log(state);
+    },
+  },
 });
